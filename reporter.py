0c9b4ad9d8ed93c1abd879d3bdb0455ae8b8dd62
btr260 <65568934+btr260@users.noreply.github.com>
2020-06-02 21:02:39 -0400

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -3,13 +3,13 @@ import csv
 import pandas
 import os
 
-#csv_filename = "sales-201710.csv"  # a relative filepath
-#csv_filepath=os.path.join("data", csv_filename)
-
-
+# PULL IN DATA
 sales = pandas.read_csv("sales-201710.csv")
 
-total_sales = sales["sales price"].sum()
+# FAILED ATTEMPT TO USE THE CSV AND OS PACKAGE
+
+#csv_filename = "sales-201710.csv"  # a relative filepath
+#csv_filepath=os.path.join("data", csv_filename)
 
 #with open(csv_filename, "r") as csv_file:  # "r" means "open the file for reading"
 #    reader = csv.DictReader(csv_filename)  # assuming your CSV has headers
@@ -18,6 +18,7 @@ total_sales = sales["sales price"].sum()
 #        #print(row["date"], row["product"], row["unit price"], row["units sold"], row["sales price"])
 #        sales.append(dict(row))
 
+# FUNCTIONS
 def to_usd(my_price):
     """
     Converts a numeric value to usd-formatted string, for printing and display purposes.
@@ -30,5 +31,10 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
-print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
+
+# CODE
+total_sales = sales["sales price"].sum()
+
+
+print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2017...")
 print("TOTAL SALES: "+to_usd(total_sales))
